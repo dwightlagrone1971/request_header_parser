3e0d59f45fa1bfd6d84c2a0ce9fdc1d0ac0ca77a
Dwight LaGrone <dwightlagrone@yahoo.com>
2017-03-10 22:49:18 +0000

    Final commit

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 var express = require('express');
+var requestIp = require('request-ip');
+var acceptLanguage = require('accept-language');
+var useragent = require('useragent');
 
 var app = express();
 
 var port = process.env.PORT;
 
+app.use(requestIp.mw());
+
+var results = {};
+
+app.use(function(req, res) {
+    var ip = req.clientIp;
+    acceptLanguage.languages(['en-US', 'zh-CN']);
+    var lang = acceptLanguage.get('en-GB,en;q=0.8,sv');
+    var agent = useragent.parse(req.headers['user-agent']);
+     // 'Mac OSX 10.8.1' 
+    
+    results = {
+        'ipaddres': ip,
+        'language': lang,
+        'software': agent.os.toString()
+    };
+    res.send(results);
+});
+
+
+
+
 app.get('/', function(req, res) {
-    res.send("This is the first web app");
+    
+  
 });
 
 
